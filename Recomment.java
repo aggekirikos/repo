bb9cf8ea116c5d953e1c4891f545742e38f3749a
Vasiliki Papasideri <t8210115@aueb.gr>
2022-11-16 19:59:25 +0200

    Add Recomment.java

---
+++ Recomment.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Recomment /*extends Comment*/ {
+    private static int recommentId;
+    private String recContent;
+    //private User recFrom;
+
+    public Recomment(String content /*, User from*/) {
+        super();
+        recContent = content;
+        recommentId++;
+        //recFrom = from;
+    }
+    public void setRecContent(String newContent){
+        recContent = newContent;
+    }
+    public void editRecomment(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("PLease enter the new comment that will replace the current one");
+        String a = input.nextLine();
+        recContent = a;
+    }
+    public String getRecContent(){
+        return recContent;
+    }
+
+    /*public void deleteRec(User currentUser) {
+        //if ( currentUser = super.from){
+        recContent= null;
+        // }
+    }*/
+}
