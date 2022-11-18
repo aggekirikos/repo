8dfb192f346aae0940fd5cb34aad760e9cbe22e1
Vasiliki Papasideri <t8210115@aueb.gr>
2022-11-18 14:22:16 +0200

    Modify Comment.java

--- Comment.java
+++ Comment.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Comment {
+
+    //Comment comment
     protected static int commentId;
     protected String commentContent;
     protected Recomment[] recomments;
-    //protected String from;
-    //protected String to;
-    public Comment(){}
-    public Comment( String content /*, String from, Post to*/){
+    protected String from;
+    protected String to;
+    public Comment( String content , String from, String to){
         commentId++;
         commentContent = content;
         /*this.from = from;
@@ -26,37 +27,23 @@ public class Comment {
         return /*from.getUser + */  " comment the post number " + /*to.getPost*/
                 ". The comment is : " + getCommentContent();
     }
-    public void delete(/*User currentUser*/){
-        //if (currentUser = from){
-        commentContent = null;
-        // }
-        //Η κλάσση Post θα εχει εναν πίνακα Comment comments
-        //και στην Post πρέπει να γίνεται η ολίσθιση
-        //σπρώχνω δηλαδη στην θέση του null το commentId,
-        //του επόμενου στοιχείου
-        //if(commentContent[id] = null){
-        //   for(int j = id; j < comments.length - 1 ή αριθμός που θα δηλώνει τα null περιεχόμενα; j++){
-        //      if (commentContent[j] = null){
-        //          break;
-        //      }
-        //      commentContent[j] = commentContent[j+1];
-        //   }
-        //   commentContent[comments.length - 1] = null;
-        // }
-    }
-    /*protected void fixRecomments(){
-        if( recomments.length != 0 ) {
-            int i = 0;
-            while (recomments[i].getRecContent() != null) {
-                i++;
+  /* public class Comment {
+        int commentId;
+        String content;
+        String [100][2] recomments;
+        public void setRecomment(String from){
+            int a=0
+            for (a ; a< recomments.length; a++){
+                if(recomments[a][1] == null){
+                    break;
+                }
             }
-            int nullPosition = i;
-            for(int j = i; j < recomments.length - 1; j++){
-                String s = recomments[j+1].getRecContent();
-                recomments[j].setRecContent(s);
-            }
-            int a = recomments.length;
-            recomments[a] = null;
+            Scanner input = new Scanner(System.in);
+            System.oyt.print("Enter the recomment : ");
+            recomment[a][1] = input.nextLine();
+            recomment[a][2] = from;
         }
-    }*/
+  }
+  * */
+
 }
