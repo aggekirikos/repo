11af159eccdd1f51529ec02577a05e4d37efad8f
Eyaggelos Siamantouras <t8210203@aueb.gr>
2023-01-10 00:55:46 +0200

    apply some changes

--- src/test/java/fOrk/DBconTest.java
+++ src/test/java/fOrk/DBconTest.java
@@ -2,39 +2,24 @@ package fOrk;
 
 import static org.junit.Assert.assertTrue;
 import java.sql.*;
-import org.junit.Test; // for @Test
-import org.junit.Before; // for @Before
-import org.junit.After; // for @After
-/**
-* Unit Test on DBcon class. This class checks the connectivity
-* of the DBcon class with the database, by making a connection
-* and creating a statement. When the connection is executed
-* successfully, both the connection and the statement are
-* closed.
-*/
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
  public class DBconTest {
     Connection connection;
     Statement statement;
-/**
-* This method makes the connection with the database before
-* the Test method.
-*/
+
     @Before
     public void before() {
         connection = DBcon.openConnection();
     }
-/**
-* This method closes the connection with the database after
-* the Test method.
-*/
+
     @After
     public void after() {
         DBcon.closeConnection(connection);
     }
-/**
-* This method creates a statement to check the actual
-* connection, and afterwards closes it.
-*/
+
     @Test
     public void closeStatementShouldCloseStatement() {
         try {
